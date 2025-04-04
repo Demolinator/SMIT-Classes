8e4bda302a23cef8bebb5d7648fc91f5f7ea652d
Talal <ta234567801@gmail.com>
2025-04-04 18:10:23 +0500

    Classes added

---
+++ Batch - 13/Class - 04-04-2025/app.js
@@ -0,0 +1,37 @@
+// regular function (traditional function declaration)
+
+// function changeText (name, age = 100) {
+//     console.log("Hello " + name + "! " + "My age is " + age + ".");
+// }
+
+// changeText("Talal");
+
+
+// function changeText () {
+//    document.getElementById("Text").innerText = "Wow! you have changed the text.";
+// }
+
+// let resetText = "Click the button to show super powers.";
+
+// function superPower (powers) {
+//     if (powers === "reset") {
+//         document.getElementById("superpower").innerText = resetText; 
+//     } else {
+//         document.getElementById("superpower").innerText = "superpower is: " + powers;
+//     }
+// }
+
+
+// anonymous function (without a name)
+// const changeText = function () {
+//     document.getElementById("Text").innerText = "Text changed";
+// }
+
+// changeText()
+
+// callback function
+// const setAlarm = function (time, callback) {
+//     setTimeout(() => {
+//         callback("Wake up! Alarm is of.")
+//     }, time)
+// }
