9a4b728a217289241cc45cd4d86118c73abcec87
Talal Ahmed <ta234567801@gmail.com>
2025-04-28 11:04:27 +0500

    Class 11 added

---
+++ SMIT-Batch16/CSS/Class - 10 - Layouting/app.js
@@ -0,0 +1,27 @@
+// let user = {
+//   id: "46eb7078-1f99-4b29-88d4-74e0473283ca",
+//   email: "bilal@gmail.com",
+//   password: "12345678",
+
+//   // phone: "+923059898769"
+// };
+
+// let cart = {
+//   uid: "46eb7078-1f99-4b29-88d4-74e0473283ca",
+//   order: [],
+//   billingAddress: "",
+//   phone: "",
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
