d8d0111b116294a61edf38e7f0b1d4f2bc55bc72
Talal <ta234567801@gmail.com>
2025-06-23 17:50:35 +0500

    Class Added Javascript 2

--- Batch - 16 - 4 to 6/Javascipt/Class - 01/app.js
+++ Batch - 16 - 4 to 6/Javascipt/Class - 01/app.js
@@ -22,6 +22,7 @@
 
 // Arthmatic operators
 // + - * / %
+// sum, concatination
 
 // BODMAS
 
---
+++ Batch - 16 - 4 to 6/Javascipt/Class - 02/app.js
@@ -0,0 +1,99 @@
+// incrementation, decrementation
+// ++ --
+
+// modulus
+//  2 % 3
+
+// Datatypes
+// string
+// "" ''
+// number
+// 12 0 9 9.5
+// array
+// []
+
+
+
+// let favDish = prompt("What is user favourite dish?", "Chicken Tikka");
+
+
+
+// console.log(favDish);
+
+
+// let num1 = +prompt("Enter a Number: ");
+
+
+// console.log(num1 + 2);
+
+
+// prompt()
+
+// 3 x 1 = 3
+// 3 x 2 = 6
+
+
+
+// let userInput = prompt("Enter a number!");
+
+// console.log(userInput + " x " + 1 + " = " + userInput * 1);
+// console.log(userInput + " x " + 2 + " = " + userInput * 2);
+// console.log(userInput + " x " + 3 + " = " + userInput * 3);
+// console.log(userInput + " x " + 4 + " = " + userInput * 4);
+// console.log(userInput + " x " + 5 + " = " + userInput * 5);
+// console.log(userInput + " x " + 6 + " = " + userInput * 6);
+// console.log(userInput + " x " + 7 + " = " + userInput * 7);
+// console.log(userInput + " x " + 8 + " = " + userInput * 8);
+// console.log(userInput + " x " + 9 + " = " + userInput * 9);
+// console.log(userInput + " x " + 10 + " = " + userInput * 10);
+
+
+// let num1 = 4;
+// let num2 = 4;
+
+// console.log(num1++); post incrementation
+// console.log(++num2); pre incrementation
+// console.log(num1--); post decrementation
+// console.log(--num1); pre decrementation
+
+
+//           4     +    5   +    5   +    4   +   6
+// console.log(num1++ + ++num2 + num1++ + --num2 + num1++);
+
+
+// Maaz 25
+// Zain 29
+// 12 37
+// 3 26
+// 4 39
+
+// let num1 = 4;
+// let num2 = 4;
+
+//            4    +    5   +    6   +   5    +   6    +    5   +    6
+// console.log(num1++ + ++num2 + ++num1 + num2++ + num1++ + --num2 + --num1);
+
+// console.log(num1);
+
+
+// console.log(12 % 3);
+
+// let initialNumber = 0;
+// let userInput = +prompt("Enter a number!");
+
+// console.log(userInput + " x " + ++initialNumber + " = " + userInput * initialNumber);
+// console.log(userInput + " x " + ++initialNumber + " = " + userInput * initialNumber);
+// console.log(userInput + " x " + ++initialNumber + " = " + userInput * initialNumber);
+// console.log(userInput + " x " + ++initialNumber + " = " + userInput * initialNumber);
+// console.log(userInput + " x " + ++initialNumber + " = " + userInput * initialNumber);
+// console.log(userInput + " x " + ++initialNumber + " = " + userInput * initialNumber);
+// console.log(userInput + " x " + ++initialNumber + " = " + userInput * initialNumber);
+
+
+// let fruits = ["Mango", "Banana", "Apple", "Kivi", "Orange", "Strawberry"];
+// let spckFruit = fruits[1] = "Pineapple";
+// let addFruit = fruits[6] = "Banana";
+
+// let addSpckFruit = fruits[10] = "Pakistani";
+
+// console.log(fruits);
