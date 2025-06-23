b5a3d724c119e0b0a3f356d1ad61d8d10d4ad7e2
Talal <ta234567801@gmail.com>
2025-06-23 11:08:29 +0500

    javascript Class added

---
+++ SMIT-Batch16 - 9 to 11/Javascript/Class - 02/app.js
@@ -0,0 +1,87 @@
+// Datatypes
+// String
+// "" ''
+// numbers
+// 0 9 5.5
+
+// built-in Functions
+// console.log()
+// Alert()
+// prompt()
+
+// variables
+// let const
+
+// arthmetic operators
+//  + - * /
+// sum, concatination
+
+// Incrementation
+// ++ --
+
+
+// let userName;
+// userName = "Talal Ahmed";
+
+// const firstName = "Talal";
+
+
+// console.log(userName, firstName);
+
+
+// alert("this is your passkey: " + 23452);
+
+// BODMAS
+
+// let num1 = 5;
+// let num2 = "9";
+
+//  
+// console.log((num1 + num2) * num2 + num1 + num2 - num2);
+
+
+// let favDish = prompt("what is your faviourite dish: ", "Chicken Tikka");
+
+// console.log(favDish);
+
+// let num1 = +prompt("Enter a Number: ");
+// let num2 = 4;
+// let sum = num1 * num2;
+
+// console.log(sum);
+
+// let num = 2;
+
+// console.log(num++); post incrementation
+// console.log(++num); pre incrementation
+// console.log(num--); post dicrementation
+// console.log(--num); pre dicrementation
+
+//                 1  +   1   +  2    +    2  +   3   +  3
+// console.log(--num + num++ + num++ + --num + ++num + num++);
+
+// console.log(num);
+
+
+
+// 3 x 1 = 3
+// 3 x 2 = 6
+
+// let firstNum = 1;
+// let userInput = prompt("Enter a number");
+
+// console.log(userInput + " x " + firstNum + " = " + userInput * firstNum);
+// console.log(userInput + " x " + ++firstNum + " = " + userInput * firstNum);
+// console.log(userInput + " x " + ++firstNum + " = " + userInput * firstNum);
+// console.log(userInput + " x " + ++firstNum + " = " + userInput * firstNum);
+// console.log(userInput + " x " + ++firstNum + " = " + userInput * firstNum);
+// console.log(userInput + " x " + ++firstNum + " = " + userInput * firstNum);
+// console.log(userInput + " x " + ++firstNum + " = " + userInput * firstNum);
+// console.log(userInput + " x " + ++firstNum + " = " + userInput * firstNum);
+// console.log(userInput + " x " + ++firstNum + " = " + userInput * firstNum);
+// console.log(userInput + " x " + ++firstNum + " = " + userInput * firstNum);
+// console.log(userInput + " x " + ++firstNum + " = " + userInput * firstNum);
+// console.log(userInput + " x " + ++firstNum + " = " + userInput * firstNum);
+// console.log(userInput + " x " + ++firstNum + " = " + userInput * firstNum);
+// console.log(userInput + " x " + ++firstNum + " = " + userInput * firstNum);
+// console.log(userInput + " x " + ++firstNum + " = " + userInput * firstNum);
