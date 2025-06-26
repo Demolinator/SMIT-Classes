07347494658b19ccbb6f21b4eedee97087c4f09d
Talal <ta234567801@gmail.com>
2025-06-26 22:11:40 +0500

    Javascript class Lab 4 to 6 added

---
+++ SMIT-Batch16-Lab 4 to 6/Javascript/Class - 01/app.js
@@ -0,0 +1,84 @@
+// Datatype
+// objects
+// {} 
+// Category, entity
+//    []   ,  {}
+
+// document.write("Hello From JavaScript!");
+
+
+// user name
+// welcome username !
+
+// let userName = prompt("Enter your Name: ");
+
+// alert("Welcome " + userName + "!");
+
+
+// 2 input 2 numbers
+// addition document 
+
+// let firstInput = +prompt("Enter First Number");
+// let scndInput = +prompt("Enter Second Number");
+
+// document.write(firstInput + scndInput);
+
+// document.write(+prompt("Enter a number") + +prompt("Enter a second number"))
+
+
+// array [] cars 
+// Last Car Type change
+
+// let cars = ["Toyota", "Honda", "Hundai", "porsche", "BMW"];
+
+// let spckCar = cars[4] = "Ferrari";
+// let addCar = cars[5] = "Audi";
+
+// console.log(cars);
+
+
+
+// user name 
+// hobbies 5
+// Enter your most favourite Hobby
+// Hello username your favourite hobby is: hobby
+// Print hobbies
+
+// let userName = prompt("Enter Your Name");
+// let usrHobby = prompt("Enter Your Most Favourtie Hobby");
+// let usrHobby1 = prompt("Enter Your second Hobby");
+// let usrHobby2 = prompt("Enter Your third Hobby");
+// let usrHobby3 = prompt("Enter Your fourth Hobby");
+// let usrHobby4 = prompt("Enter Your fifth Hobby");
+
+
+// let userInputs = [usrHobby, usrHobby1, usrHobby2, usrHobby3, usrHobby4];
+
+// console.log("Hello " + userName + " Your Favourite hobby is: " + userInputs[0]);
+
+// console.log(userInputs);
+
+
+// let Toyota = { 
+//     Model: "Corolla", 
+//     Price: 4700000, 
+//     Color: Black
+// }
+
+
+// let human = {
+//     name: "Khizer",
+//     fathersName: "Muhammad Javed",
+//     age: 21
+// }
+
+// console.log(human);
+
+// let human = {
+//     name: "Khizer",
+//     fathersName: "Muhammad Javed",
+//     age: 21
+// }
+
+// console.log(human.age);
+
