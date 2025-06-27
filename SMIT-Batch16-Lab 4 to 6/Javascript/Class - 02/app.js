99debc0a9c971c97448bd340fa167eae394e1eb4
Talal <ta234567801@gmail.com>
2025-06-27 17:39:56 +0500

    Javascript Class Added

---
+++ SMIT-Batch16-Lab 4 to 6/Javascript/Class - 02/app.js
@@ -0,0 +1,44 @@
+// ------------- OBJECT
+
+// Human
+
+// .dot notation
+// [] this notation will only look for variables declared and initialized
+
+// let obj = "key 1";
+
+// let human = {
+//     Name:"Khizar", 
+//     age:21, 
+//     clothes: "kameez Shalwar", 
+//     rollNo: 324244, 
+//     cnic: 4220132238283,
+// };
+
+
+// console.log(human[obj] = "karachi", human);
+
+
+
+
+// let arr = [];
+// 5 prompts from user "Enter a Item"
+// console.log()
+// let i = 0;
+// --
+
+let arr = [];
+
+arr[0] = prompt("Enter a Item");
+arr[1] = prompt("Enter a Item");
+arr[2] = prompt("Enter a Item");
+arr[3] = prompt("Enter a Item");
+arr[4] = prompt("Enter a Item");
+
+let i = 5;
+
+console.log(arr[--i]);
+console.log(arr[--i]);
+console.log(arr[--i]);
+console.log(arr[--i]);
+console.log(arr[--i]);
