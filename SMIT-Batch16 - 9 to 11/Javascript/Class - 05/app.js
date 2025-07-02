e6a26e4da405bcf75454ddc46105448b4765135e
Talal <ta234567801@gmail.com>
2025-07-02 11:07:15 +0500

    Javascript Class 05 added

---
+++ SMIT-Batch16 - 9 to 11/Javascript/Class - 05/app.js
@@ -0,0 +1,91 @@
+// function sum (num1, num2) {
+//     console.log(num1 + num2);
+// };
+
+// sum(2, 2);
+// sum(5, 10);
+
+
+// function sum (num1, num2) {
+//     console.log(num1 + num2);
+// };
+
+// let userInput = +prompt("Enter a number");
+// let userInput1 = +prompt("enter second number");
+
+// sum(userInput, userInput1);
+
+
+
+// function user input
+// firstName and lastName
+// welcome fullName !
+
+
+// function userName () {
+//     let userInput = prompt("enter your first name");
+//     let userInput1 = prompt("enter your first name");
+//     console.log("welcome " + userInput +" "+ userInput1 + "!")
+// }
+
+// userName();
+
+
+// function user () {
+//     return "welcome user!"
+// }
+
+
+// let userfunc = user();
+
+// console.log(userfunc);
+
+
+
+// function greet (userName) {
+//     return "welcome " + userName + "!";
+// }
+
+// function welcomeUser (name) {
+//     let message = greet(name);
+//     console.log(message);
+// }
+
+
+// welcomeUser("Talal");
+
+// 2 functions user input 2 numbers sum
+
+// function sum (num1, num2) {
+//     return num1 + num2;
+// }
+
+// function add (num1, num2) {
+//     let message = sum(num1, num2)
+//     console.log(message);
+// }
+
+// let userNumber = +prompt("Enter a number");
+// let userNumber1 = +prompt("Enter second number");
+
+// add(userNumber, userNumber1)
+
+
+// 1 functions pizza toppings 
+// 2 functions base
+// console.log(2function())
+
+// function topping (toppings) {
+//     return toppings + " toppings";
+// }
+
+// function pizza (base, topping1, topping2) {
+//     let Topping = topping1;
+//     let Topping1 = topping2;
+//     let fullTopping = Topping + " " + Topping1;
+//     let prompt = topping(fullTopping);
+    
+//     return "your "+ base + " pizza with " + prompt;
+// }
+
+// console.log(pizza("Cheese crust", "pepperoni", "Mushrooms"))
