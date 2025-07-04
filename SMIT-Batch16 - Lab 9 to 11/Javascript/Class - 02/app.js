d43c1e6de3b7f74161c765c6e780c62f95fb6e14
Talal <mohammadsaadali786.92@gmail.com>
2025-07-04 10:58:59 +0500

    Javascript class added lab 9 to 11 functions Dom

---
+++ SMIT-Batch16 - Lab 9 to 11/Javascript/Class - 02/app.js
@@ -0,0 +1,107 @@
+// 2 functions pizza 
+// 1 function makeTopping(topping) return topping + " topping";
+// 2 function makePizza(base, topping1, topping2) print 
+// your Pizza base with topping1 and topping2 is ready!
+// console.log(makePizza("Cheese Crust", "Mushrooms", "Chicken"));
+// user prompt
+
+
+// function makeTopping (topping) {
+//     return topping + " topping";
+// }
+
+// function makePizza (base, topping1, topping2) {
+//     let fullTopping1 = makeTopping(topping1);
+//     let fullTopping2 = makeTopping(topping2);
+//     // let baseOfPizza = base;
+//     return "your Pizza " + base + " with " + fullTopping1 + " and " + fullTopping2 + " is ready!";
+// }
+
+// let userBase = prompt("Enter the base of the Pizza");
+// let userTopping = prompt("Enter the first topping for the pizza");
+// let userTopping1 = prompt("Enter the second topping for the pizza");
+
+
+// console.log(makePizza(userBase, userTopping, userTopping1));
+
+
+
+
+
+
+// function sum (num1, num2) {
+//     return num1 + num2;
+// }
+
+// function add (num1, num2) {
+//     let message = sum(num1, num2)
+//     console.log(message);
+// }
+
+// add(2, 2);
+
+
+
+// 3 function add, subtract, multiply
+// 4 function finalAnswer();
+// userInput 5 userInput + 7 
+// console.log();
+// 5 + 7
+// 5 - 7
+// 5 * 7
+
+// function add (userInput) {
+//     return userInput + 7
+// }
+
+// function subtract (userInput) {
+//     return userInput - 7
+// }
+
+// function multiply (userInput) {
+//     return userInput * 7
+// }
+
+// function finalAnswer (userInput) {
+//     console.log(add(userInput));
+//     console.log(subtract(userInput));
+//     console.log(multiply(userInput));
+// }
+
+// let userInput = +prompt("Enter a number");
+// //          undefined 
+// finalAnswer(userInput);
+
+
+
+
+
+
+
+
+
+
+// let h1 = document.getElementById("change-text");
+// h1.innerHTML = "Hello From JS";
+// console.log(h1);
+
+// p "paragraph from javascript";
+
+
+// let p = document.getElementById("change-para");
+// let pInner = p.innerHTML = "paragraph from javascript";
+// console.log(pInner);
+
+
+// function changeText () {
+//     let h1 = document.getElementById("change-text");
+//     h1.innerHTML = "Hello From JS";
+// }
+
+// function changePara () {
+//     let button = document.getElementById("change-para");
+//     button.innerHTML = "paragraph from javascript";
+//     console.log(button);
+// }
+
+
