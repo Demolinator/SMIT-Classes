0e51d1a301494b0bd12107c78c9d92d0289b61c5
Talal <ta234567801@gmail.com>
2025-07-07 17:41:13 +0500

    Class Javascript Added

---
+++ Batch - 16 - 4 to 6/Javascipt/Class - 05/app.js
@@ -0,0 +1,185 @@
+// comparision operator
+// < less than
+// > greater than
+// <= less than and eaquals to
+// >= greater than and eaquals to
+// === equality operator
+// !== not equals to
+
+
+// console.log(18 > 17) // true
+// console.log(18 > 20) // false
+
+// console.log(18 < 20) // true
+// console.log(18 < 15) // false
+// console.log(18 < 18) // false
+
+// console.log(18 >= 19) // false
+// console.log(18 >= 18) // true
+
+// console.log(18 <= 18) // true
+// console.log(18 <= 15) // false
+
+
+
+// let age = 18;
+
+// if (age > 18) {
+//     console.log("you are eligible for CNIC");
+// }
+
+// let fullName = "Talal";
+
+
+// if (fullName === "Talal") {
+//     console.log("welcome user!")
+// }
+// else {
+//     console.log("go home!");
+// }
+
+
+// user prompt Enter your Percentage
+// A+ 90 
+// A  80 
+// B  70
+// C  60
+// D  50
+// E  40
+// F  30 you have failed
+
+// let mark =+prompt("enter a number");
+
+// if(mark >= 90){
+//     console.log("A+");
+// }
+// else if(mark >= 80){
+//     console.log("A");
+// }
+// else if(mark >= 70){
+//     console.log("B");
+// }
+// else if(mark >= 60){
+//     console.log("C");
+// }
+// else if(mark >= 50){
+//     console.log("D");
+// }
+// else if(mark > 30){
+//     console.log("E");
+// }
+// else{
+//     console.log("You have failed");
+// }
+
+
+
+
+// let religion = "Islam";
+// let userReligion = prompt("Enter your religion");
+
+// if (userReligion !== religion) {
+//     console.log("you are not allowed.");
+// }
+// else {
+//     console.log("you get the visa.");
+// }
+
+
+
+// let account = 40000;
+// let userAmount = +prompt("Enter your amount to transfer");
+
+// if (userAmount <= account) {
+//     console.log("Transfer successful!");
+// }
+// else {
+//     console.log("insufficent balance!");
+// }
+
+
+
+// user pin !==
+
+// let userPin = +prompt("Enter your pin");
+// let pin = 4345;
+
+// if (userPin !== pin) {
+//     console.log("Wrong Credentials");
+// }
+// else {
+//     console.log("Login Successful!");
+// }
+
+
+
+
+// && And operator kurkure aur cola next 
+// || OR operator kurkure yah cola next
+
+// let shop = "Kurkure";
+// let shop1 = "colanext";
+
+// if (shop === "Kurkure" && shop1 === "colanext") {
+//     console.log("le aao!");
+// }
+// else {
+//     console.log("Darwaza band milega!");
+// }
+
+
+
+
+
+// let shop = "colanext";
+// let shop1 = undefined;
+
+// if (shop === "Kurkure" || shop1 === "colanext") {
+//     console.log("le aao!");
+// }
+// else {
+//     console.log("Darwaza band milega!");
+// }
+
+
+
+
+// user input
+// let meal = meal;
+// meal biryani and cola
+// console.log("Aae maze")
+// console.log("Soup le ao!")
+
+// let input = prompt("enter the food here");
+// let input1 = prompt("enter the food here");
+// let input2 = prompt("enter the name");
+// let meal = "biryani";
+// let drink = "cola";
+// let myName = "talal";
+
+// if (input === meal && input1 === drink && input2 === myName) {
+//     console.log("Aae maze")
+// }
+// else {
+//     console.log("Soup le ao!")
+// }
+
+
+
+
+
+// you will bring laptop or mobile if you want to attempt the quiz if not you will be not allowed to attempt the quiz.
+
+// let userMobile = prompt("Do you have mobile");
+// let userLaptop = prompt("Do you have laptop");
+
+// if (userMobile === "Available" || userLaptop === "Available") {
+//     console.log("you can attempt the quiz");
+// }
+// else {
+//     console.log("you can't attempt the quiz");
+// }
+
+// if (0) {
+//     console.log("Hello from JS");
+// }
