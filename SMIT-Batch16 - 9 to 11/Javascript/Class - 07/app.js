3abceb53a93977bbc796fb8543ffa5ab2e7620c2
Talal <ta234567801@gmail.com>
2025-07-09 11:18:17 +0500

    Javacript class 7 added 9 to 11

---
+++ SMIT-Batch16 - 9 to 11/Javascript/Class - 07/app.js
@@ -0,0 +1,168 @@
+// Data Types
+// variables
+// functions
+// if, else if
+// comparison operator
+// operators
+// DOM document.getElementById("")
+// innerHTML property
+
+// && And kurkure aur cola next
+// || Or  kurkre yah cola next
+
+
+// create a condition and take two numbers from the user and then print the greater number if user entered zero then tell him you entered zero in the DOM.
+
+// let num1 = +prompt("enter the first number: ")
+// let num2 = +prompt("enter the second number: ")
+// let n1 = document.getElementById("change-text")
+
+// if (num1 === 0 || num2 === 0) {
+//     n1.innerHTML = "you have entered zero"
+// } 
+// else if (num1 > num2) {
+//     n1.innerHTML = num1
+// }
+// else {
+//     n1.innerHTML = num2
+// }
+
+
+
+// let shop1 = "Kurkure";
+// let shop2 = "pepsi";
+
+
+// if (shop2 === "Kurkure" || shop1 === "Kurkure") {
+//     console.log("aajao!");
+// }
+// else {
+//     console.log("ghar mat ana!");
+// }
+
+
+
+
+
+// factory employee if id card present and notExpired allow him in the factory if expired say to him to renew his card DOM
+
+// let card = prompt("you have the card!")
+
+// // let employe = +prompt("enter a year in your card!")
+
+// let notExpired = true;
+
+// let h1 =document.getElementById("change-text")
+
+// if (card === "yes" && notExpired) {
+//     h1.innerHTML = "you are allowed"
+// }
+// else {
+//     h1.innerHTML = "please renew the card"
+// }
+
+
+
+
+// let variables 3 you will have three numbers greater number
+
+// let x = 20;
+// let y = 10;
+// let z = 9;
+
+// let h1 =document.getElementById("change-text")
+
+// if (x > y && x > z) {
+//     h1.innerHTML = "x is greater"; 
+// }
+// else if (y > x && y > z) {
+//     h1.innerHTML = "y is greater";
+// }
+// else {
+//     h1.innerHTML = "z is greater";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// let cities = ["Karachi", "Islamabad", "Multan", "peshawar"];
+
+
+// // cities.push("Rawalpindi");
+// // cities.push("Lahore");
+
+// // cities.unshift("Balochistan");
+
+// // cities.pop();
+// // cities.shift();
+
+
+
+// // cities.splice(2, 2);
+
+
+// console.log(cities.length);
+// console.log(cities)
+
+
+
+
+
+// indexOf() -1, Banana 1, indexOF("Kiwi")
+
+
+// let fruits = ["Apple", "Banana", "Kiwi", "Mango", "Banana"];
+
+// console.log(fruits.indexOf("Pineapple"));
+
+
+
+
+
+// let fruits = ["Apple", "Banana", "Kiwi", "Mango", "Banana"];
+
+// fruits.splice(2, 2, "Pineapple", "Strawberry")
+
+// console.log(fruits);
+
+
+
+
+// create an array of students altleast 3 and print one of the user that exists in the array if the user does not exist print student does not exist desired student ek student Kazim
+
+
+
+
+
+// arr.indexOf("asif");
+
+// if(arr[0] === "asif"){
+
+//     console.log("user exist");
+//     arr.splice(2,0,"kazim")
+// }
+// else{
+    //       console.log("user does not exist");
+    // }
+    // console.log(arr)
+    
+
+// let arr = ["asif" , "amir" , "kamil"];
+    
+// if (arr.indexOf("Mubeen") !== -1) {
+//     console.log("User exists!");
+//     console.log(arr.indexOf("amir"));
+//     arr.splice(1, 0, "Kazim");
+//     console.log(arr);
+// }
+// else {
+//     console.log("user does not exist");
+// }
