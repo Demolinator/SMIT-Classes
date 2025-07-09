1125f2e656a414fb68fb7699a14714e57176044a
Talal <ta234567801@gmail.com>
2025-07-09 17:45:31 +0500

    Javascript class added: 4 to 6 lab

---
+++ SMIT-Batch16-Lab 4 to 6/Javascript/Class - 04/app.js
@@ -0,0 +1,99 @@
+// Data Types String, Numbers, boolean, array, object
+// variables let, const
+// functions built-in functions: console.log(), alert(), Prompt(), document.wirte()
+// operators +, -, *, /, %, >, <, >=, <=, &&, ||
+// DOM document.getElementById("")
+// innerHTML property
+
+
+
+
+// Take two numbers from the user and print the greater number in the DOM if the user enters 0 in either prompts the print you have entered Zero
+
+
+// let num1 = +prompt("Enter your number");
+// let num2 = +prompt("Enter your number");
+// let h1 = document.getElementById("text");
+
+// if (num1 === 0 || num2 === 0) {
+//     h1.innerHTML = "You have entered Zero";
+// }
+// else if(num1 > num2) {
+//     h1.innerHTML = num1 + " is greater";
+// }
+// else {
+//     h1.innerHTML = num2 + " is greater";
+// }
+
+
+
+// declare and initialize three variables and in one variable keep a larger number and then print the larger number in DOM
+
+// let a = 213;
+// let b = 52;
+// let c = 30;
+// let h1 = document.getElementById("text");
+
+// if (a > b && a > c) {
+//     h1.innerHTML = "a is greater";
+// }
+// else if (b > a && b > c) {
+//     h1.innerHTML = "b is greater";
+// }
+// else {
+//     h1.innerHTML = "c is greater";
+// }
+
+
+
+
+// let number = 34; create a condition that will check if the number is even and greater than 100 it is, odd and greater 100, less than 100 or equals to 100 12 % 2 = 0 
+
+// let number = 1025;
+// //                   aur
+// if (number % 2 === 0 && number > 100) {
+//     console.log("the number is even and greater than 100!");
+// }
+// else if (number % 2 !== 0 && number > 100) {
+//     console.log("the number is odd and greater than 100!");
+// }
+// else {
+//     console.log("the number is less than 100 or equals to 100!");
+// }
+
+
+
+
+
+
+
+// Methods: push, pop, shift, unshift, indexOF
+// Property: length
+
+
+// indexOF("")
+// -1 when not available
+// if duplicate the first will be printed
+
+
+// let cities = ["Karachi", "Lahore", "Islamabad", "Peshawar"];
+
+// cities.push("Multan");
+// cities.push("Quetta");
+
+// console.log(cities);
+
+
+
+// let fruits = ["Apple", "Banana", "Kiwi", "Apple","Strawberry"];
+
+// fruits.push("Mango");
+
+// fruits.unshift("Pineapple");
+
+// fruits.pop();
+// fruits.shift();
+
+// console.log(fruits);
+
+// console.log(fruits.indexOf("Apple"));
