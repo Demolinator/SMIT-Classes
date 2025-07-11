e813ae04bf0798e384ce4867ed46720b352c0925
Talal <ta234567801@gmail.com>
2025-07-11 17:47:29 +0500

    Javascript Class added: 4 to 6 audi

---
+++ Batch - 16 - 4 to 6/Javascipt/Class - 06/app.js
@@ -0,0 +1,110 @@
+// let fruits = ["Apple", "Banana", "Pineapple", "Kiwi"];
+
+// fruits.splice(2, 2);
+// console.log(fruits);
+
+
+
+
+// let fruits = ["Apple", "Banana", "Pineapple", "Kiwi"];
+
+// fruits.splice(2, 0, "Mango", "Strawberry");
+// fruits.pop();
+// fruits.splice(2, 1);
+// // console.log(fruits);
+
+// let h1 = document.getElementById("text");
+// h1.innerHTML = fruits;
+
+
+
+
+
+// clean cities check how many items in the array and print the items in the DOM
+
+
+
+// let cleanCities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Multan"];
+// let h1 = document.getElementById("text");
+// h1.innerHTML = cleanCities.length + " " + cleanCities;
+
+
+
+
+
+// enter you city? city exists is in the array and then print you live in a clean city or you do not live in a clean city
+
+// let userCityName = prompt("enter a clean city");
+// let cleanCities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Multan"];
+// let h1 = document.getElementById("text");
+// //                0                   
+// if (cleanCities.indexOf(userCityName) >= 0){
+//    h1.innerHTML = "you live in a clean city"
+//    console.log(cleanCities.indexOf(userCityName))
+// }
+// else{
+//     h1.innerHTML = "you do not live in a clean city"
+//     console.log(cleanCities.indexOf(userCityName))
+// }
+
+
+
+
+
+// 1, 3, 4, 5, 6, 1
+// first and last numbers in the array are equal
+
+
+// let numbers = [1, 3, 4, 5, 6, 1];
+
+// if (numbers[0] === numbers[numbers.length - 1]) {
+//     console.log('first and last items are equal');
+// }
+// else {
+//     console.log("first and last items are not equal")
+// }
+
+
+
+
+
+// let students and check if the length of the user array is 5 and if it is not 5 then print seats are available if not then print seats are full
+
+// let userName = prompt("Enter Name")
+
+// let students = ["Talal", "Bilal", "Ishaq", "Ikhlas", "Irfan"];
+
+// if (students.length < 5) {
+//     students.splice(2, 0, userName)
+//     console.log("Seats are available", students);
+// }
+// else {
+//     console.log("seats are full", students);
+// }
+
+
+
+
+// your password exceeds the limit 6 is the limit
+// password accepted
+
+// let userPassword = prompt("Enter your Password?");
+
+// if (userPassword.length <= 6) {
+//     console.log("password accepted")
+// }
+// else {
+//     console.log("your password exceeds the limit 6 is the limit")
+// }
+
+
+
+
+
+
+
+
+
+// for (let i = 0; i < 5; i++) {
+//     console.log("Hello World", i);
+// }
