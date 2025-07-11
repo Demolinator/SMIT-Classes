ef786d344de0806b58d9bd0b04e3342108e4c013
Talal <ta234567801@gmail.com>
2025-07-11 11:23:27 +0500

    javascript Added: 9 to 11 Added

---
+++ SMIT-Batch16 - Lab 9 to 11/Javascript/Class - 03/app.js
@@ -0,0 +1,104 @@
+// let fruits = ["Apple", "Banana", "Pineapple", "Apple"];
+
+// console.log(fruits.indexOf("Apple", "Pineapple"));
+
+
+// DOM Method: document.getElementById("");
+// DOM property: .innerHTML
+
+
+// if two numbers are equal output DOM
+
+// let num1 = +prompt("Enter a number to check equality");
+// let num2 = +prompt("Enter a number to check equality");
+// let print = document.getElementById("text");
+
+// if (num1 === num2) {
+//     print.innerHTML = "Numbers are equal"
+// }
+// else {
+//     print.innerHTML = "Numbers are not equal"
+// }
+
+
+
+// check if the number is between 10 and 20 then print DOM
+// the number is between 10 and 20
+// the number is not between 10 and 20
+
+
+// let userInput = +prompt("Enter a number");
+// let print = document.getElementById("text");
+
+// if (userInput >= 10 && userInput <= 20) {
+//     print.innerHTML = "the number is between 10 and 20";
+// }
+// else {
+//     print.innerHTML = "the number is not between 10 and 20";
+// }
+
+
+
+
+
+// if password length is under the limit of 6 digits
+// Password exceeds the limit
+// password is acceptable
+
+
+
+// let password = "123456";
+// let print = document.getElementById("text");
+
+// if (password.length <= 6) {
+//     print.innerHTML = "password is acceptable";
+// }
+// else {
+//     print.innerHTML = "Password exceeds the limit";
+// }
+
+
+
+// take user input and then check if the number is even and above 100 and check if the number is odd and above 100 else print the number is less than 100
+
+
+
+// let userInput = +prompt("Enter your number?");
+// let print = document.getElementById("text");
+
+// if (userInput % 2 === 0 && userInput > 100) {
+//     print.innerHTML = "the number is even and above 100";
+// }
+// else if (userInput % 2 !== 0 && userInput > 100) {
+//     print.innerHTML = "the number is odd and above 100";
+// }
+// else {
+//     print.innerHTML = "the number is less than 100 or equals to 100";
+// }
+
+
+
+
+
+
+// console.log("bring print of one page of this pdf");
+// console.log("bring print of one page of this pdf");
+// console.log("bring print of one page of this pdf");
+// console.log("bring print of one page of this pdf");
+// console.log("bring print of one page of this pdf");
+// console.log("bring print of one page of this pdf");
+// console.log("bring print of one page of this pdf");
+// console.log("bring print of one page of this pdf");
+// console.log("bring print of one page of this pdf");
+// console.log("bring print of one page of this pdf");
+
+
+// i would have been sleeping
+// console.log("bring print of each page of the pdf");
+
+
+
+
+for (let i = 0; i < 5; i++) {
+    console.log("Hello World", i);
+}
