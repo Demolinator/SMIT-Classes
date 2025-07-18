5d8e1f73177a3c5d7ef4230c051e26479e096911
Talal <ta234567801@gmail.com>
2025-07-18 11:04:14 +0500

    Javascript class 9 to 11 lab tasks added

---
+++ SMIT-Batch16 - Lab 9 to 11/Javascript/Class - 04/app.js
@@ -0,0 +1,169 @@
+// user will tell how many times the for loop should iterate
+
+// let userInput = prompt("Enter how many times the loop should run?");
+
+// for (let i = 0; i < userInput; i++) {
+//     console.log(i);
+// }
+
+
+
+
+
+// for (let i = 10; i > 0; i--) {
+//     console.log(i);
+// }
+
+
+
+
+
+// 5 plate biryani
+// 1 bottle pepsi
+// 1 pastry
+
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log("you have eaten " + i + " number of plates")
+//     for (let j = 1; j <= 2; j++) {
+//         console.log("you have drank " + j + " number of bottles");
+//         for (let k = 1; k <= 1; k++) {
+//             console.log("you have eaten " + k + " amount of pastries");
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+// 3 X 3 square * with using a star
+
+// for (let i = 1; i <= 10; i++) {
+//     let row = "";
+//     for (let j = 1; j <= i; j++) {
+//         row = row + "* ";
+//     }
+//     console.log(row);
+// }
+
+
+
+
+// print even numbers with for loop till 50
+
+
+// for (let i = 0; i <= 1000; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+
+// [1,2,3,4,5,6,7,8,9,10,11,12,13]
+// count 
+
+// let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+// let greaterCount = 0;
+// let lessCount = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 10) {
+//         greaterCount++;
+//     }
+//     else {
+//         lessCount++;
+//     }
+// }
+
+// console.log("numbers greater than 10 are: " + greaterCount);
+// console.log("numbers less than 10 are: " + lessCount);
+
+
+
+
+
+
+
+
+
+// let fruits = ["Apple", "Banana", "Kiwi", "Watermelon"];
+
+// // fruits.splice(1, 1, "strawberry");
+// let favFruits = fruits.slice(1, 3);
+// favFruits.pop();
+
+// console.log(favFruits, fruits);
+
+
+
+
+/*
+.length
+
+push()
+pop()
+shift()
+unshift()
+indexOf()
+splice()
+
+
+slice()
+*/ 
+
+
+// let firstName = 23242;
+
+// console.log(firstName.slice(3));
+
+
+
+
+// .toUpperCase()
+// .toLowerCase()
+
+
+
+// let firstName = prompt("enter your first name?");
+
+// // let upperName = firstName.toUpperCase()
+// // let upperName = firstName.toLowerCase()
+// let upperName = firstName[0].toUpperCase();
+// let lowerName = firstName.slice(1).toLowerCase();
+
+
+
+// console.log(upperName + lowerName);
+
+
+
+
+
+
+
+
+// title case: Talal Ahmed
+// Lorem Ipsum Dolor Sit, Amet Consectetur adipisicing elit. Sint quo at amet sunt iusto, pariatur beatae nostrum repellendus, ratione eum blanditiis maxime rem? Autem cum eaque, debitis consequuntur libero blanditiis?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 0; i < 5; i++) {
+
+// }
