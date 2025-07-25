c3358c9d7132c7b200bb6cc9375066e2382773c9
Talal <ta234567801@gmail.com>
2025-07-25 17:50:42 +0500

    Javascript Class Added: 4 to 6

---
+++ Batch - 16 - 4 to 6/Javascipt/Class - 10/app.js
@@ -0,0 +1,98 @@
+// ["ali", "KhIZer", "Mustafa", "MoiZ"]
+
+// let names = ["ali", "KhIZer", "Mustafa", "MoiZ", "IrFaN"];
+
+// for (let i = 0; i < names.length; i++) {
+//     let name = names[i];
+//     let firstLetter = name.charAt(0).toUpperCase();
+//     let rest = name.slice(1).toLowerCase();
+//     console.log(firstLetter + rest);
+// } 
+
+
+
+
+
+// let message = "This is a stupid Product"; 
+// "****" 
+
+
+// let message = "This is a stupid Product. you guys are stupid.";
+// message = message.replace(/stupid/g, "****");
+// console.log(message);
+
+
+
+
+
+
+
+
+// let para = "a quick World War II brown fox jumps over the lazy dog.a quick World War II brown fox jumps over the lazy dog.a quick World War II brown fox jumps over the lazy dog.a quick World War II brown fox jumps over the lazy dog.a quick World War II brown fox jumps over the lazy dog.a quick World War II brown fox jumps over the lazy dog.a quick World War II brown fox jumps over the lazy dog.a quick World War II brown fox jumps over the lazy dog.";
+
+// // let textToReplaceWith = "The second world war";
+// // for(let i = 0; i < para.length; i++){
+//     //     let char = para.slice(i, i + 12);
+//     //     if (char === "World War II") {
+//         //         //  8 "a quick "        + "The second w..." + 8 + 12 = 20 
+//         //         para = para.slice(0, i) + textToReplaceWith + para.slice(i + 12);
+//         //     }
+//         // }
+//         // console.log(para);
+    
+    
+// let textToReplaceWith = "The second world war";
+// para = para.replace(/World War II/g, textToReplaceWith);
+// console.log(para);
+
+
+
+// let str = "This is a sentence.";
+//
+
+
+// let str = "This is a sentence.";
+// let count = 0;
+
+// for (let i = 0; i < str.length; i++) {
+//     if (str.charAt(i) === " ") {
+//         count++;
+//     }
+// };
+
+// console.log("The number of counts are: ", count + 1);
+
+
+
+
+
+
+
+
+
+// string "alsafadfadfa asdf adsfAS";
+// 
+// let str = "alsafadfadfa asdf adsfAS";
+// for (let i = 0; i < str.length; i++) {
+//     let chr = str[i];
+//     if (chr >= "a" && chr <= "z") {
+//         console.log("This is in lower case: ", chr)
+//     }
+//     else if (chr >= "A" && chr <= "Z") {
+//         console.log("This is in upper case: ", chr)
+//     }
+//     else {
+//         console.log("This is not an alphabet. ", chr)
+//     }
+    
+// }
+
+
+
+
+
+// let password checker 
+// password >= 6, Uppercase, number
+// this password is strong
+// this password is weak
+
