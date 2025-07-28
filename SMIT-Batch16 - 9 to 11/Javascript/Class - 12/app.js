08091a68ba2684e9c980fae21b108a0f67dfdd50
Talal <ta234567801@gmail.com>
2025-07-28 11:05:53 +0500

    Javascript Class Added 9 to 11

---
+++ SMIT-Batch16 - 9 to 11/Javascript/Class - 12/app.js
@@ -0,0 +1,221 @@
+// let randomNumber = Math.random();// 0.111111111111, 0.99999999999
+
+// // 0.1 = 0
+// // 0.5 = 1
+// // 0.8 = 1
+// let roundedNumber = Math.round(randomNumber);
+// console.log(roundedNumber);
+
+// let randomNumber = Math.random();
+// let floorNumber = Math.floor(randomNumber);
+// console.log()
+
+// Math
+
+// Math.random() 0.111, 0.999
+// Math.round() 0.1 = 0; 0.5 = 1; 0.9 = 1;
+// Math.floor() 0.1 = 0; 0.5 = 0; 0.9 = 0; 0 = 0;
+// Math.ceil() 0.1 = 1; 0.2 = 1; 0.9 = 1; 0 = 0;
+
+// console.log(Math.pow(2, 3)); 2 X 2 X 2
+// console.log(Math.sqrt(81)); 2 X 2 X 2
+
+// let randomNumber = Math.floor((Math.random() * 6) + 1);
+// console.log(randomNumber);
+
+// coin toss
+// Math.random()
+
+// let toss = Math.random(); // 0.1, 0.99
+
+// if (toss < 0.5) {
+//     console.log("Heads", toss);
+// }
+// else {
+//     console.log("Tails", toss);
+// }
+
+// celcius to farenheight
+// (celcius * 9/5) + 32
+
+// let celsius = +prompt("Enter the celsius");
+// let farenheight = (celsius * 9/5) + 32
+// console.log(farenheight);
+
+// let a = 2;
+// let b = 5;
+// let c = 7;
+
+// console.log(Math.max(a, b, c));
+// console.log(Math.min(a, b, c));
+
+// let number = 2;
+// console.log((number + 2).toString());
+
+// if (a > b && a > c) {
+//     console.log("a is greater")
+// }
+// else if (b > a && b > c) {
+//     console.log("b is greater")
+// }
+// else {
+//     console.log("c is greater")
+// }
+
+// let toss = Math.random();
+// let toss1 = Math.random();
+// let toss2 = Math.random();
+
+// console.log(toss, toss1, toss2);
+
+// OTP
+// 4-digits
+// Math.random()
+// Math.floor()
+
+// let otp = Math.floor((Math.random() * 9999) + 1000);
+// console.log(otp);
+
+// console.log(Math.abs(5)); // 5
+// console.log(Math.abs(-2.11)); // 5
+
+// let number = 99;
+
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+// console.log(number + " bottles of beer on the wall, " + number-- + " bottles of beer.")
+
+// let number = 99;
+
+// for (let i = 99; i >= -25; i--) {
+//     console.log(i + " bottles of beer on the wall, " + i + " bottles of beer.")
+// }
+
+// 5, a = 1;
+// 2, b = 1;
+// 2, c = 1;
+
+// let cleanCities = ["Karachi", "Lahore", "Islamabad", "Quetta"];
+// let userCity = prompt("Enter your city?");
+
+
+// for (let i = 0; i < cleanCities.length; i++) {
+//     if (cleanCities[i] === userCity) {
+//         console.log("you live in a clean city!");
+//         break;
+//     }
+//     else {
+//         console.log("you do not live in a clean city!");
+//         break;
+//     }
+// }
+
+
+// if (cleanCities[0] === userCity) {
+//   console.log("You live in a clean city!");
+// } else if (cleanCities[1] === userCity) {
+//   console.log("You live in a clean city!");
+// } else if (cleanCities[2] === userCity) {
+//   console.log("You live in a clean city!");
+// } else if (cleanCities[3] === userCity) {
+//   console.log("You live in a clean city!");
+// } else {
+//   console.log("You do not live in a clean city!");
+// }
