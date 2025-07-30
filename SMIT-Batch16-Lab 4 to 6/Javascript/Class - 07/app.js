ba5388e5b691db042cefc9a64f055b27c4429b5b
JaffarAman <mohammadsaadali786.92@gmail.com>
2025-07-30 17:43:25 +0500

    Javascript class added 4 to 6 Lab

---
+++ SMIT-Batch16-Lab 4 to 6/Javascript/Class - 07/app.js
@@ -0,0 +1,123 @@
+// ticket 4560
+// lottery
+// congratulations! you won a tour to Karachi
+// Sorry! you won a tour to Turkey
+
+// let ticket = 8;
+// let number = Math.floor((Math.random() * 10) + 1);
+// if (ticket === number) {
+//     console.log("congratulations! you won a tour to Karachi " + number);
+// }
+// else {
+//     console.log("Sorry! you won a tour to Turkey " + number);
+// }
+
+
+
+// let number = "234.45KG";
+// console.log(Number(number));
+// console.log(ParseInt(number));
+// console.log(ParseFloat(number));
+
+
+// console.log(isNaN("23KG"));
+
+
+
+
+
+// "499.99RS"
+// Rs
+
+// let price = "499.99RS";
+// let priceNum = parseFloat(price);
+// console.log("Rs ", priceNum);
+
+
+
+// weight = "80.6574KG";
+
+// let weight = "80.6574KG";
+// weight = parseInt(weight);
+// console.log(weight, "Kg");
+
+
+// let age = prompt("Enter your age?"); 23 years old
+// invalid charchter
+// 18 >= you are eligible to vote
+// 18 <= you are not eligible to vote
+
+// let age = prompt("enter your age?");
+// age = parseInt(age);
+// if(isNaN(age)) {
+//     console.log("Invalid Charchter");
+// }
+// else if (age >= 18) {
+//     console.log("you are eligible to vote")
+// }
+// else {
+//     console.log("you are not eligible to vote")
+// }
+
+
+
+
+
+// let speed = "80Km/h";
+// 70 <= no challans
+// 70 > Pay heavy Fines
+
+
+// let speed = "250km/h";
+// speed = parseInt(speed);
+// if (speed <= 70) {
+//     console.log("no challans");
+// } 
+// else {
+//     console.log("Pay heavy Fines");
+// }
+
+
+
+
+// let salary = 50000;
+// let bonus = "12.5%";
+// bonusAmount
+
+// let salary = 50000;
+// let bonus = "12.5%";
+// let bonusNum = parseFloat(bonus)
+// let calcBonus = salary * (bonusNum / 100);
+
+// console.log((calcBonus + salary) - 20000);
+
+
+// let discount = "70%";
+// let product  = "23000TV";
+// let product1  = "20000Mobile";
+// let product1  = "40000Laptop";
+
+
+// let discount = "70%";
+// let product  = "23000TV";
+// let product1  = "20000Mobile";
+// let product2  = "40000Laptop";
+
+// let productPercent = parseFloat(product) * (parseFloat(discount) / 100);
+// let productDicount = parseFloat(product) - productPercent;
+
+// let productPercent1 = parseFloat(product1) * (parseFloat(discount) / 100);
+// let productDicount1 = parseFloat(product1) - productPercent;
+
+// let productPercent2 = parseFloat(product2) * (parseFloat(discount) / 100);
+// let productDicount2 = parseFloat(product2) - productPercent;
+
+// console.log(productDicount.toFixed(2), productDicount1.toFixed(2), productDicount2);
+
+
+
+
+// let bill = prompt("Enter your bill?");
+// let people = prompt("Enter the amount of person?");
+// 
+
