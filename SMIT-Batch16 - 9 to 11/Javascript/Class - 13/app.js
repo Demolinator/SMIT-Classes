e8ea0b66894a7ce8b50a2200c44c6f2dc646442c
Talal <ta234567801@gmail.com>
2025-07-30 10:57:44 +0500

    Javascript class added 9 to 11

---
+++ SMIT-Batch16 - 9 to 11/Javascript/Class - 13/app.js
@@ -0,0 +1,130 @@
+// let number = "234.45";
+// let number1 = "234px";
+// let number2 = "px";
+
+// console.log(parseFloat(number1));
+// console.log(parseInt(number1));
+
+
+// let age = "23 years old"; years of age
+// let age = "23 years old";
+// console.log(parseInt(age) + " years of age");
+
+
+
+
+// let discount = "499.99";
+// console.log(parseFloat(discount));
+
+
+// let salary = 50000;
+// let bonus = "12.5%";
+// let bonusNum = parseFloat(bonus);
+// let calculatedBonus = salary * (bonusNum / 100);
+// console.log(calculatedBonus + salary);
+
+
+
+// let discount = "70%";
+// let product = "23000TV";
+// let product1 = "20000Mobile";
+// let product2 = "40000Laptop";
+// let discount = "70%";
+// let product = "23000TV";
+// let product1 = "20000Mobile";
+// let product2 = "40000Laptop";
+
+// console.log(Number(discount));
+// let discountNum = parseFloat(discount);
+// let calcPrdct =  parseFloat(product) * (discountNum / 100);
+// let calcPrdct1 =  parseFloat(product1) * (discountNum / 100);
+// let calcPrdct2 =  parseFloat(product2) * (discountNum / 100);
+
+
+// console.log(parseFloat(product) - parseInt(calcPrdct));
+// console.log(parseFloat(product1) - parseInt(calcPrdct1));
+// console.log(parseFloat(product2) - parseInt(calcPrdct2));
+
+
+// console.log(isNaN(true));
+
+
+
+// let age = prompt("Enter your age"); 23 years old
+// 18 you are eligible 
+// 15 you are not elegible 
+// Invalid Charachter
+
+// let age = prompt("Enter your age");
+// age = parseInt(age);
+// // console.log(age);
+
+// if (isNaN(age)) {
+//     console.log("Invalid Charachter");
+// }
+// else if (age >= 18) {
+//     console.log("you are eligible to vote");
+// }
+// else {
+//     console.log("you are not eligible to vote");
+// }
+
+
+
+// weight / (height * height)
+
+// let weight = "80KG";
+// let height = "1.67m";
+
+
+// let weight = prompt("Enter your weight?");
+// let height = prompt("Enter your height?");
+// weight = parseFloat(weight);
+// height = parseFloat(height);
+
+// let bmi = weight / (height * height);
+
+// console.log(bmi.toFixed(2));
+
+
+
+// let marks = prompt; 
+// invalid charachter
+// 80 >= A
+// 70 >= b
+// 60 >= c
+// 50 >= fail
+
+// let marks = prompt("enter your marks?");
+// marks = parseInt(marks);
+
+// if (isNaN(marks)) {
+//     console.log("Invalid Charachter");
+// }
+// else if (marks >= 80 && marks <= 100) {
+//     console.log("It's a full A: " + marks);
+// }
+// else if (marks >= 70 && marks <= 100) {
+//     console.log("It's a full B: " + marks);
+// }
+// else if (marks >= 60 && marks <= 100) {
+//     console.log("It's a full C: " + marks);
+// }
+// else if(marks <= 50 && marks <= 100) {
+//     console.log("It's a full F: " + marks + " Mabrook!");
+// }
+// else {
+//     console.log("Invalid Marks");
+// }
+
+
+
+
+// let bill = prompt("Enter your bill?"); 
+// let people = prompt("Enter the number of person?");
+
+
+
+
+
+
