50a9f16e2fb06ddd8675c499bc6ce7de3e589496
Talal <ta234567801@gmail.com>
2025-08-06 17:51:27 +0500

    Javasctipt class added 4 to 6 Lab

---
+++ SMIT-Batch16-Lab 4 to 6/Javascript/Class - 09 Date Object/app.js
@@ -0,0 +1,52 @@
+// let rightNow = new Date();
+// let today = rightNow.getDay();// 0 - 6
+// let days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+// let dayStr = days[today];
+// console.log(dayStr);
+
+
+
+// let rightNow = new Date();
+
+// let thisMonth = rightNow.getMonth();
+// let months = ["jan", "feb", "marc", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
+// let MonthStr = months[thisMonth];
+// console.log(MonthStr);
+
+
+// let rightNow = new Date();
+
+// let todayDate = rightNow.getDate();
+// let thisYear = rightNow.getFullYear();
+// let currentHour = rightNow.getHours();
+// let currentMins = rightNow.getMinutes();
+// let currentSeconds = rightNow.getSeconds();
+// let currentMiliSeconds = rightNow.getMilliseconds();
+// console.log(currentHour + ":" + currentMins + ":" + currentSeconds + ":" + currentMiliSeconds); 
+
+
+
+// let rightNow = new Date();
+
+// let currentHour = rightNow.getHours();
+// let currentMins = rightNow.getMinutes();
+// let currentSeconds = rightNow.getSeconds();
+// let currentMiliSeconds = rightNow.getMilliseconds();
+
+// if (currentHour > 12) {
+//     currentHour = currentHour - 12;
+// }
+
+// console.log(currentHour + ":" + currentMins + ":" + currentSeconds + ":" + currentMiliSeconds); 
+
+
+
+
+// let nextBirthDay = new Date("20 july, 2026");
+// let rightNow = new Date();
+
+// let bdayDateMiliSeconds = nextBirthDay.getTime(); 
+// let currentDateMiliSeconds = rightNow.getTime(); 
+// let leftDaysBday = (bdayDateMiliSeconds - currentDateMiliSeconds) / (1000 * 60 * 60 * 24);
+
+// console.log(Math.floor(leftDaysBday));
