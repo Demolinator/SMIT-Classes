217ac43fc4861111ee95bc1063869f4d7f84c6ec
HasanCoder66 <hasanashraf874@gmail.com>
2025-08-13 11:11:47 +0500

    Javascript Class added 9 to 11

---
+++ SMIT-Batch16 - 9 to 11/Javascript/Class - 16 Local Storage and Events/app.js
@@ -0,0 +1,27 @@
+// localStorage.setItem("studentName", "Talal");
+
+// let storedName = localStorage.getItem("studentName");
+// console.log(storedName);
+
+
+
+let stdntName = document.getElementById("student-name");
+let rollNo = document.getElementById("roll-no");
+let allStudents = [];
+
+function submit () {
+    let student = {
+        "studentName" : stdntName.value,
+        "rollNo" : rollNo.value
+    }
+
+    let storedObj = localStorage.getItem("studentInfo");
+
+    if (storedObj) {
+        allStudents = JSON.parse(storedObj);
+    }
+
+    allStudents.push(student);
+    localStorage.setItem("studentInfo", JSON.stringify(allStudents))
+    console.log(storedObj);
+}
