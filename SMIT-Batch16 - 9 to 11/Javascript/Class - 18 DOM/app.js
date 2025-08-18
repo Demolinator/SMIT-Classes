ed2efb90defebdd1148bccbed6d191159512560f
Talal <ta234567801@gmail.com>
2025-08-18 11:08:51 +0500

    javascript class added 9 to 11

---
+++ SMIT-Batch16 - 9 to 11/Javascript/Class - 18 DOM/app.js
@@ -0,0 +1,30 @@
+// let pEl = document.getElementById("special-para");
+// let pEls = document.getElementsByClassName("special");
+
+// for (let i = 0; i < pEls.length; i++) {
+//     pEls[1].style.backgroundColor = "lightgreen"
+//     pEls[1].style.color = "red";
+//     pEls[1].style.fontSize = "20px";
+//     pEls[1].style.fontStyle = "italic";
+// }
+
+
+
+
+// let pEls = document.getElementsByTagName('p')
+
+// for (let i = 0 ; i < pEls.length; i++) {
+    // pEls[i].innerHTML = "This is a paragraph <br /> <h1><em>Written by Talal Ahmed</em></h1>";
+    // pEls[i].innerText = "This is a paragraph <br /> <h1><em>Written by Talal Ahmed</em></h1>";
+// }
+
+// console.log(pEls);
+
+
+
+
+// 1 show three cards
+// 2 tell user to click on king
+// 3 generate a random number ranging from 1 to 3
+// 4 check the card clicked and then the generated card number
+// 5 show the king card if selected print in the DOM You Win in green if lost print You lose in red
