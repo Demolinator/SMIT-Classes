e7b7ea72070848b8a3ea2f6141dd520f3a91c040
Talal <ta234567801@gmail.com>
2025-08-22 17:31:54 +0500

    JavaScript class 4 to 6 Lab Added 22/08/2025

---
+++ SMIT-Batch16-Lab 4 to 6/Javascript/Class - 13 DOM/app.js
@@ -0,0 +1,37 @@
+// function moveForward () {
+//     console.log("played")
+// }
+
+// let time = setTimeout(moveForward, 100);
+
+
+
+// let lastPEl = document.getElementById("last-para");
+// let allPara = document.getElementsByTagName("p");
+let classSpecialEl = document.getElementsByClassName("special");
+let isChanged = false;
+// console.log(allPara[0].hidden = true);
+
+function changeStyle() {
+    if (!isChanged) {
+        for (let i = 0; i < classSpecialEl.length; i++) {
+            classSpecialEl[i].style.backgroundColor = "black";
+            classSpecialEl[i].style.color = "white";
+            classSpecialEl[i].style.fontSize = "32px";
+            classSpecialEl[i].innerHTML = "This is para no. <br />" + i;
+            isChanged = true;
+        }
+    } else {
+        for (let i = 0; i < classSpecialEl.length; i++) {
+            classSpecialEl[i].style.backgroundColor = "white";
+            classSpecialEl[i].style.color = "black";
+            classSpecialEl[i].style.fontSize = "16px";
+            classSpecialEl[i].innerHTML = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam tempora quisquam fuga dignissimos maxime nam explicabo, modi facilis ea voluptatibus ab ex adipisci impedit sequi incidunt consequatur facere delectus quis.";
+            isChanged = false;
+        }
+    }
+}
+// console.log("This is para no. \n" + 1);
+
+
+setInterval(changeStyle, 1000);
