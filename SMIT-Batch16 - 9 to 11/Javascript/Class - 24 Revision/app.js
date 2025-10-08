2424be254d26eb96796b0aeefa54ba4ae5601966
Talal <ta234567801@gmail.com>
2025-10-08 11:04:23 +0500

    Javascript revision class and firebase hosting addeed

---
+++ SMIT-Batch16 - 9 to 11/Javascript/Class - 24 Revision/app.js
@@ -0,0 +1,50 @@
+// primitive Data Types
+// stack immutable
+// There are 7 primitive data types in javascript
+
+// '', "" string
+// 12, 3.45, -30 numbers
+// true, false bolean logical yes or no
+// let name; undifned create a vairable and leave it empty
+// let fullName = null; null create a variable and intentionally keep it empty
+// symbol("id")  unique or hidden identifer added
+// 9999999999999999n bigInt 16 9007199254740991 
+
+
+// non primitive
+// heap mutable
+// object {"name": "Talal Ahmed"} 
+// array ["apple", "banana", "Kiwi"]
+// function greet () {}
+
+
+
+// primitive data types code
+
+// let a = 10;
+// let b = a;
+// b = 20 
+// console.log(a);
+
+// // stack
+// a => 10
+// b => 20
+
+
+// non primitve data types
+
+// let person = {
+//     name: "Mubeen",
+//     age: 19,
+//     isStudent: true
+// }
+
+// let person1 = person;
+// person1.name = "Zaheer"
+
+// console.log(person)
+
+
+// heap
+// person => Ref 23x43
+// person1 => Ref 23x43
