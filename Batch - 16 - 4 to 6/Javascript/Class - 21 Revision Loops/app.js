9e895e5de49d98e012e43a13c0c41391ba617cc3
Talal <ta234567801@gmail.com>
2025-10-20 09:33:51 +0500

    Javascript class added 9 to 11

--- Batch - 16 - 4 to 6/Javascript/Class - 21 Revision Loops/app.js
+++ Batch - 16 - 4 to 6/Javascript/Class - 21 Revision Loops/app.js
@@ -29,9 +29,9 @@ let arr = [2, 4, 8, 3, 5];
 for (let i = 0; i < arr.length - 1; i++) {
     for (let j = 0; j < arr.length - 1 - i; j++) {
         if (arr[j] > arr[j + 1]) {
-            let temp = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = temp;
+            let temp = arr[j]; // 4
+            arr[j] = arr[j + 1]; // 3
+            arr[j + 1] = temp; // 4
             console.log(arr,i,j);
         }
     }
