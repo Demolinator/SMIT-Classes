b2593ad494b9ba4c3c508a3fd8c0dc4cc71cb800
hasanCoder66 <hasanashraf874@gmail.com>
2025-10-24 11:06:21 +0500

    Javascript Class Added 9 to 11 Lab: Expense Tracker

---
+++ SMIT-Batch16 - Lab 9 to 11/Javascript/Class - 15 Revision Fast Food App/app.js
@@ -0,0 +1,41 @@
+// function greet () {}
+// 
+// let foodList = document.getElementById("food");
+
+// console.log(foodList);
+
+
+// let groupEls = document.getElementsByClassName("group");
+
+// groupEls[0].style.color = "red"; 
+// groupEls[1].style.color = "red"; 
+// groupEls[2].style.color = "red"; 
+
+// console.log(groupEls)
+
+
+
+// let foodListEl = document.getElementById("food");
+// let qty = document.getElementById("qty");
+// let bill = document.getElementById("bill");
+// let price = 0;
+
+// // total = foodListEl * qty
+
+// function placeOrder () {
+//     if (foodListEl.value === "pizza") {
+//         price = 1500;
+//     } else if (foodListEl.value === "burger") {
+//         price = 500;
+//     } else {
+//         price = 700;
+//     }
+
+//     let total = price * qty.value;
+//     if (qty.value <= 0) {
+//         alert("not a valid qty.");
+//         return;
+//     }
+
+//     bill.innerHTML = "you ordered: " + foodListEl.value + " " + qty.value + "(s) " + " total: " + total;
+// }
