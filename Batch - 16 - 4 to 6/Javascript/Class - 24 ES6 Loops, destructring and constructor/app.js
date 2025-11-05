178c51f870036b143c5c3f55f64a56caf47175aa
Talal <ta234567801@gmail.com>
2025-11-05 19:03:04 +0500

    Javascript class added: 4 to 6 loops destructuring

---
+++ Batch - 16 - 4 to 6/Javascript/Class - 24 ES6 Loops, destructring and constructor/app.js
@@ -0,0 +1,94 @@
+// let password;
+// while (password !== "1234") {
+//     password = prompt("Enter your password?");
+// }
+// console.log("Access Granted")
+
+
+// console.log("1234" !== "1234");
+
+
+// let network;
+// let connected;
+// do {
+//     connected = Math.ceil(Math.random() * 2);
+//     network = Math.ceil(Math.random() * 2);
+//     console.log("trying...", connected, network);
+// }
+// while (network !== connected);
+
+// console.log("connected!", connected, network);
+
+
+// Destructuring
+
+
+
+// let arr = ["Apple", "Banana", "Kiwi"];
+
+// let [, , fruit3] = arr;
+
+// console.log(fruit2, fruit3);
+
+
+
+// let obj = {
+//     carName: "BMW",
+//     series: "m5",
+//     door: {
+//         canOpenAbove: true
+//     }
+// }
+
+// let { carName, series, door:{ canOpenAbove } } = obj;
+
+// console.log(carName, series, canOpenAbove);
+
+// let arr = ["Apple", "Banana", "Kiwi", {isOk: true}];
+
+// let [, , , {isOk}] = arr;
+
+// console.log(isOk = false, arr);
+
+
+
+
+// Class constructor
+
+// class Students {
+//     constructor (fullName, rollNo) {
+//         this.fullName = fullName;
+//         this.rollNo = rollNo;
+//     }
+//     attendance = false;
+
+//     canWalk(feedBack) {
+//         return feedBack;
+//     }
+// }
+
+// let student1 = new Students("Moiz", "123465");
+// let student2 = new Students("Nouman", "123546");
+
+// console.log(student1.canWalk, student2.canWalk("yes with difficulty!"));
+
+
+
+
+// let arr = "Apple";
+
+// for (let fruits of arr) {
+//     console.log(fruits);
+// }
+
+// for (let fruits in arr) {
+//     console.log(fruits)
+// }
+
+
+
+
+
+import { add, subtract } from "./myLibrary/myFunctions.js"
+
+console.log(add(2, 5), subtract(2, 5));
