cd03c07aefae9e74ad23adb79d9441ea6cf7aae8
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2025-11-12 17:56:53 +0500

    Javascript class added 4 to 6 lab

---
+++ SMIT-Batch16-Lab 4 to 6/Javascript/Class - 22 loops/app.js
@@ -0,0 +1,83 @@
+// let arr = [255, 67, 78, 45];
+// let total = 0;
+
+// for (let nums of arr) {
+//     total += nums;
+// }
+
+// console.log(total);
+
+
+// let str = "Talal";
+// let reversed = "";
+
+// for (let letter of str) {
+//     reversed = letter + reversed;
+// }
+
+// console.log(reversed);
+
+
+
+
+
+// let count = 5;
+// for of, *do while*, for in 
+// console.log("game started!")
+
+
+// setTimeout(() => {
+//     console.log(`game will start soon`)
+//     setTimeout(() => {console.log(`Bas hogaya!`)
+//         setTimeout(() => {console.log(`bas 5 minute aur`)
+//             setTimeout(() => {console.log(`yaar masla aagaya thora sa!`)
+//                 setTimeout(() => console.log(`Mazak ker rha tha kuch nhi start ho rha hai.`), 1000);
+//             }, 10000);
+//         }, 12000);
+//     }, 5000);
+// }, 1000);
+
+
+// do {
+//     console.log(`The Game Starts in ${count}`);
+//     count--
+// }
+// while (count > 0);
+
+// console.log(`Game Started!`);
+
+
+
+
+// let ratings = [1, 3, 2, 2, 0];
+// // *for of*, for in, do while
+// // total / ratings
+// let total = 0;
+
+// for (let r of ratings) total += r;
+
+// let avg = 0;
+// let average = () => avg = total / ratings.length;
+// average()
+// console.log(`your resturants average rating is: ${avg}`);
+
+
+
+// let sales = [1200, 2000, 2300, 800, 10000];
+// // *for of*, for in, do while
+// let highiest = 0;
+
+// for (let s of sales) {
+//     if (s > highiest) highiest = s;
+// } 
+
+// console.log(`the highest sale of 5 days is: ${highiest}`);
+
+
+
+
+// () => {
+
+// }
+
+
