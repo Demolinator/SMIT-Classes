219ac2f4dab72ec8598b804d011e3a3aa63adb66
Talal <ta234567801@gmail.com>
2025-11-26 18:06:04 +0500

    4 to 6 audi

---
+++ Batch - 16 - 4 to 6/Javascript/Class - 29 Call Back, Call Bind Apply, Closures/app.js
@@ -0,0 +1,30 @@
+// function greet (worldPrint) {
+//    console.log("Hello " + worldPrint());
+// }
+
+// function world () {
+//     return "world";
+// }
+
+// greet(world);
+
+
+// Call Back Hell
+
+// setTimeout(() => {
+//   console.log("First Timeout");
+//   setTimeout(() => {
+//     console.log("Second Timeout");
+//     setTimeout(() => {
+//       console.log("Third Timeout");
+//       setTimeout(() => {
+//         console.log("Fourth Timeout");
+//         setTimeout(() => {
+//           console.log("Fivth Timeout");
+//         }, 1000);
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+
