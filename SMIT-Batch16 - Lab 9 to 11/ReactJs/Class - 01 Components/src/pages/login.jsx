a5d5d7fc765b6f6f2ab54208f4d51e8efa46ba4a
Mohammad Saad Ali <mohammadsaadali786.92@gmail.com>
2025-11-28 11:10:32 +0500

    Javascript class added 9 to 11 added

---
+++ SMIT-Batch16 - Lab 9 to 11/ReactJs/Class - 01 Components/src/pages/login.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Button from '../components/Button'
+
+function login() {
+  return (
+    <>
+      <div className='text-red-500'>Login</div>
+      <div className='text-red-500'>World</div>
+      <Button />
+    </>
+  )
+}
+
+export default login
