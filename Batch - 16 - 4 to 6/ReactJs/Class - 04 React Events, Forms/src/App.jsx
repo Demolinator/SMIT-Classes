489e8c30c6662e18c8d6701513b3cbe9b4fca03b
Talal <ta234567801@gmail.com>
2025-12-05 17:50:16 +0500

    Javascript class added 4 to 6

---
+++ Batch - 16 - 4 to 6/ReactJs/Class - 04 React Events, Forms/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+  let [message, setMessage] = useState("");
+
+  function handleSubmit (e) {
+    e.preventDefault();
+    if (password.length > 6) {
+      setMessage("Sign Up Successful! " + email + password)
+    } 
+    else {
+      setMessage("Password to short please input at least 7");
+    }
+    // console.log("clicked!", email, password)
+  }
+
+  return (
+    <>
+      <div>React Sign Up Form</div>
+
+      <form onSubmit={handleSubmit}>
+        <input className='border' type="email" onChange={(e) => setEmail(e.target.value)}/>
+        <input className='border' type="password" onChange={(e) => setPassword(e.target.value)}/>
+        <input type="submit" />
+      </form>
+
+      <p>{message}</p>
+    </>
+  )
+}
+
+export default App
