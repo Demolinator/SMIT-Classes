320097b657dab9ba10c275a52ed629b7fb62f9b7
Mohammad Saad Ali <mohammadsaadali786.92@gmail.com>
2025-12-08 18:06:30 +0500

    javascript class added 4 to 6 lab

---
+++ SMIT-Batch16-Lab 4 to 6/React Js/Class - 02 ReactJs Project, composition/src/components/auth/LoginBadge.jsx
@@ -0,0 +1,9 @@
+function LoginBadge ({ message })  {
+    return (
+        <div className="bg-green-300 absolute right-5 top-20 text-green-800 text-2xl p-5 rounded-2xl">
+           {message}
+        </div>
+    )
+}
+
+export default LoginBadge
