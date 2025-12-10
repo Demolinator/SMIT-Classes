9c9b8cab3dd8abe0d3175e9a4857ba910e358067
Talal <ta234567801@gmail.com>
2025-12-10 17:55:27 +0500

    Javascript class added 4 to 6 audi

---
+++ Batch - 16 - 4 to 6/ReactJs/Class - 05 React Hooks useEffect, Render Cycle/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import Header from './components/UI/Header'
+import Main from './components/UI/Main'
+import Footer from './components/UI/Footer'
+import './App.css'
+import LoginBadge from './components/auth/LoginBadge'
+  
+function App() {
+  // let counter = 0; 
+  // let count = () => {
+  //   counter = counter + 1
+  //   console.log(counter)
+  // }
+
+
+// ------------------------------------------------
+
+  // let [count, setCount] = useState(0);
+
+  // return (
+  //   <>
+  //     <div>Talal's React Project</div>
+  //     <p>counter: {count}</p>
+  //     <button onClick={() => {
+  //       setCount(count + 1)
+  //     }}>click me</button>
+  //   </>
+  // )
+
+
+// ------------------------------------------------
+
+
+    return (
+      <>
+      <Header />
+      <Main />
+      <Footer />
+      <LoginBadge message="Login Successful" />
+      </>
+    )   
+}
+
+export default App
---
+++ Batch - 16 - 4 to 6/ReactJs/Class - 05 React Hooks useEffect, Render Cycle/src/components/UI/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <footer>
+            <ul>
+                <li>Privacy Policy</li>
+                <li>Terms and Conditions</li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer
