800fa8a7e95ab21108f26d84bf23788be9484908
Mohammad Saad Ali <mohammadsaadali786.92@gmail.com>
2025-12-15 18:05:37 +0500

    Class added 4 to 6 lab added

---
+++ SMIT-Batch16-Lab 4 to 6/React Js/Class - 03 ReactJs SPA, and Pages/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from './Button'
+
+const Navbar = () => {
+  return (
+    <div className='w-full border-b border-[#3B4953] text-[#EBF4DD] p-2 flex justify-between items-center bg-[#5A7863] '>
+      this is the navbar
+      <Button />
+    </div>
+  )
+}
+
+export default Navbar
