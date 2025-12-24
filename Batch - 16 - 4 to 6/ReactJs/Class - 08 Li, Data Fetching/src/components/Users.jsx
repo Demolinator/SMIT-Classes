a4f4e8204d939d2368814460d1427aeb5edd96c4
Talal <ta234567801@gmail.com>
2025-12-24 18:01:43 +0500

    JAVASCRIPT CLASS ADDED 4 to 6 audi added

---
+++ Batch - 16 - 4 to 6/ReactJs/Class - 08 Li, Data Fetching/src/components/Users.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+const Users = () => {
+  let [users, setUsers] = useState([]);
+  let [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      });
+
+      console.log("run...")
+  }, []);
+
+
+  return (
+    <ol className="list-disc mx-6">
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+
+      {loading && <p>Loading....</p>}
+    </ol>
+  );
+};
+
+export default Users;
